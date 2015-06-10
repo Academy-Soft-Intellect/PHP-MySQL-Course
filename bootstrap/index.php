86d2933ef6cd66efb1b1a9a8c3b6d4c9c15686e7
Nikolai Ivanov <mraiur@gmail.com>
2015-06-11 00:35:35 +0300

    Added jquery helper callbacks for the add/remove product from/to basket buttons.

--- bootstrap/index.php
+++ bootstrap/index.php
@@ -5,10 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Web Shop</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,17 +45,6 @@
 
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   </body>
 </html>
--- bootstrap/list.php
+++ bootstrap/list.php
@@ -1,3 +1,4 @@
+
 <table class="table table-striped">
     <tr>
         <th>#</th>
@@ -12,11 +13,11 @@
         <td><img src="https://placeholdit.imgix.net/~text?txtsize=15&txt=160%C3%97160&w=100&h=70" alt="..." class="img-responsive img-thumbnail"></td>
         <td>Name</td>
         <td>
-            <button type="button" class="btn btn-info btn-sm">
+            <button type="button" data-item-id="1" class="add-to-basket btn btn-info btn-sm">
                 <i class="glyphicon glyphicon-shopping-cart"></i>
                 Add to basket
             </button>
-            <button type="button" class="btn btn-danger btn-sm">
+            <button type="button" data-item-id="1" class="remove-from-basket btn btn-danger btn-sm">
                 <i class="glyphicon glyphicon-remove"></i>
                 Remove from basket
             </button>
@@ -24,3 +25,27 @@
     </tr>
 
 </table>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".add-to-basket").click(function(){
+            var product_id = $(this).attr('data-item-id');
+
+            $.post('?controller=basket&action=add-to-basket', {
+                product_id: product_id
+            }, function(){
+                alert("Added product ("+product_id+") to basket");
+            });
+        });
+
+        $(".remove-from-basket").click(function(){
+            var product_id = $(this).attr('data-item-id');
+
+            $.post('?controller=basket&action=remove-from-basket', {
+                product_id: product_id
+            }, function(){
+                alert("Removed product ("+product_id+") from basket");
+            });
+        });
+    });
+</script>
